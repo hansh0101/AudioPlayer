f75022d3efd6d0a0f40c2fe385aa48237d971054
hansh0101 <hansh0101@naver.com>
2023-05-08 18:21:51 +0900

    UI 구현

--- app/src/main/java/com/vimosoft/audioplayer/MainActivity.kt
+++ app/src/main/java/com/vimosoft/audioplayer/MainActivity.kt
@@ -1,7 +1,19 @@
 package com.vimosoft.audioplayer
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.vimosoft.audioplayer.databinding.ActivityMainBinding
 
-class MainActivity: AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
+    // ---------------------------------------------------------------------------------------------
+    // UI 바인딩 객체
+    private lateinit var binding: ActivityMainBinding
 
+    // ---------------------------------------------------------------------------------------------
+    // 생명주기 콜백 메서드
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
 }
