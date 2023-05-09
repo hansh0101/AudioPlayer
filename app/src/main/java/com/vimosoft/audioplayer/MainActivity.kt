7615eac1f0134cf3e94160b8ebaa1b85a438ce44
hansh0101 <hansh0101@naver.com>
2023-05-09 12:05:19 +0900

    AssetManager를 사용해 음악 재생 구현

--- app/src/main/java/com/vimosoft/audioplayer/MainActivity.kt
+++ app/src/main/java/com/vimosoft/audioplayer/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.vimosoft.audioplayer
 
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.vimosoft.audioplayer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     // ---------------------------------------------------------------------------------------------
-    // UI 바인딩 객체
+    // 음악 재생을 위한 변수
+    private val mediaPlayer = MediaPlayer()
+
+    // ---------------------------------------------------------------------------------------------
+    // UI 바인딩 객체 변수
     private lateinit var binding: ActivityMainBinding
 
     // ---------------------------------------------------------------------------------------------
@@ -15,5 +21,42 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        initEventListener()
+    }
+
+    // 이벤트 리스너 등록.
+    private fun initEventListener() {
+        binding.run {
+            buttonPlay.setOnClickListener { playMusic() }
+            buttonStop.setOnClickListener { stopMusic() }
+        }
+    }
+
+    private fun playMusic() {
+        if (mediaPlayer.isPlaying) return
+
+        runCatching {
+            val assetFd = assets.openFd(FILE_NAME)
+            mediaPlayer.setDataSource(assetFd.fileDescriptor, assetFd.startOffset, assetFd.length)
+            assetFd.close()
+            mediaPlayer.run {
+                prepare()
+                start()
+            }
+        }.onFailure { Log.e("playMusic()", it.toString()) }
+    }
+
+    private fun stopMusic() {
+        if (!mediaPlayer.isPlaying) return
+
+        mediaPlayer.run {
+            stop()
+            reset()
+        }
+    }
+
+    companion object {
+        const val FILE_NAME = "music.mp3"
     }
 }
